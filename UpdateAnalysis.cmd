03ac2a9a4fcfd5dae692c7956997fd278cd946fb
hdickie <hdickie@dons.usfca.edu>
2017-11-23 23:34:32 -0800

    deleting index.html to test auto push

--- UpdateAnalysis.cmd
+++ UpdateAnalysis.cmd
@@ -27,5 +27,9 @@ IF NOT "0" == "%ERRORLEVEL%" (
 cd src
 %R_Script% -e "rmarkdown::render('Driver.Rmd',output_file='C:/Users/Hume Dickie/Desktop/Github/Senior-Project/index.html')"
 
+git add *
+git commit -m "Automated push."
+git push
+
 ::echo Finished Successfully.
 ::pause
